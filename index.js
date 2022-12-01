f9ececf738ec8b0c7513addb9bc67bdab270edf5
Juan Revelo <revelojuan88@gmail.com>
2022-12-01 17:10:39 -0500

    Trabajo en clase

---
+++ index.js
@@ -0,0 +1,13 @@
+console.clear()
+console.log("Hola mundo")
+
+
+const funcion=()=>{
+    const x=document.getElementById('id').value;
+    console.log(x)
+    const y = document.getElementById('nombre').value;
+    console.log(y)
+    const z=document.getElementById('password').value;
+    console.log(z)
+}
+
